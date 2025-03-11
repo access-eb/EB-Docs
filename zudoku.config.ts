222be2e3323db2b5bb558579ef57e0586d8edfef
Maxi Perchik <maxoperchik@gmail.com>
2025-03-11 14:04:52 -0300

    + Herd + Product Addons plugin instructions

--- zudoku.config.ts
+++ zudoku.config.ts
@@ -6,23 +6,26 @@ const config: ZudokuConfig = {
     pageTitle: "Exitbuilt",
     logo: {
       src: {
-        light: "https://i0.wp.com/exitbuilt.com/wp-content/uploads/2023/12/cropped-cropped-Asset-3.png?w=512&ssl=1",
+        light:
+          "https://i0.wp.com/exitbuilt.com/wp-content/uploads/2023/12/cropped-cropped-Asset-3.png?w=512&ssl=1",
         dark: "https://i0.wp.com/exitbuilt.com/wp-content/uploads/2023/12/cropped-cropped-Asset-3.png?w=512&ssl=1",
       },
     },
   },
 
   metadata: {
-    "title": "Exitbuilt Documentation",
-    "description": "Documentation for Exitbuilt",
-    "logo": "https://i0.wp.com/exitbuilt.com/wp-content/uploads/2023/12/cropped-cropped-Asset-3.png?w=512&ssl=1",
-    "favicon": "https://i0.wp.com/exitbuilt.com/wp-content/uploads/2023/12/cropped-cropped-Asset-3.png?w=512&ssl=1",
-    "authors": ["Mateo Marco", "Maximiliano Perchik"],
-    "creator": "Maximiliano Perchik",
+    title: "Exitbuilt Documentation",
+    description: "Documentation for Exitbuilt",
+    logo: "https://i0.wp.com/exitbuilt.com/wp-content/uploads/2023/12/cropped-cropped-Asset-3.png?w=512&ssl=1",
+    favicon:
+      "https://i0.wp.com/exitbuilt.com/wp-content/uploads/2023/12/cropped-cropped-Asset-3.png?w=512&ssl=1",
+    authors: ["Mateo Marco", "Maximiliano Perchik"],
+    creator: "Maximiliano Perchik",
   },
 
   topNavigation: [
     { id: "eb", label: "Exitbuilt" },
+    { id: "herd", label: "Herd" },
     // { id: "docs", label: "Documentation" },
     // { id: "api", label: "API Reference" },
   ],
@@ -38,7 +41,18 @@ const config: ZudokuConfig = {
       {
         type: "category",
         label: "Google Ads",
-        items: ["google_ads/google_ads", "google_ads/create_conversion", "google_ads/google_ads_conversion_to_gtm"],
+        items: [
+          "google_ads/google_ads",
+          "google_ads/create_conversion",
+          "google_ads/google_ads_conversion_to_gtm",
+        ],
+      },
+    ],
+    herd: [
+      {
+        type: "category",
+        label: "Product Addons",
+        items: ["herd/product_addons/index","herd/product_addons/customization"],
       },
     ],
   },
